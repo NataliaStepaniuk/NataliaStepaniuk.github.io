b29fcb1f1c1d24ea0ab3faa28e3174a7393e901c
Natalia Stepaniuk <neteliy84@gmail.com>
2024-04-04 19:11:33 +0300

    changes

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 const currency = (total) => parseFloat(Math.round(total * 100) / 100).toFixed(2);
 
-const compare = (key, order='asc') => (a, b) => {
+const compare = (key, order='acs') => (a, b) => {
     if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) return 0;
     
     const A = (typeof a[key] === 'string') ? a[key].toUpperCase() : a[key];
@@ -20,10 +20,8 @@ const findByProps = function(items, props, what) {
             founds.push(items[i]);
         }            
     })
-
     return founds;
 }
-
 
 const sortingOrders = [
     {key:"default", value: "Default sorting"}, 
@@ -38,6 +36,7 @@ function Product(id, name, price, image) {
     this.price = price;
     this.image = image;
 }
+
 
 function CardProduct(item) {
     
@@ -112,39 +111,88 @@ function CardProduct(item) {
         let product = productList.getProductById(id);
         product = {...product, amount: 1};
         shoppingCart.addItemToCart(product);
-        document.getElementById('cart-amount').textContent = shoppingCart.totalAmount();
+        // document.getElementById('cart-amount').textContent = shoppingCart.totalAmount();
     } 
 }
 
 function Cart(tax = 0.07, shipping = 0) {
-
+    // console.log("Cart constructor", this);
     this.tax = tax;
     this.shipping = shipping;
 
-    let cart = [];
+    const store = new Store();
+
+    let cart = store.init('basket');
 
     this.saveCart = function() {
-        console.log(cart);
+        // console.log(cart);
+        store.set('basket', cart);
+        cartAmount.textContent = shoppingCart.totalAmount();
     }
+
 
     function Item (id, price, amount) {
         this.id = id;
         this.price = price;
         this.amount = amount;
     }
+
+    const cartItemTemplate = (item, product) => `
     
+    <div class="row cart-item" id="id${product.id}">
+        <div class="cell"><img src="${product.image}" alt="${product.name}" height="30"></div>
+        <div class="cell">${product.name}</div>
+        <div class="cell"><span class="product-price price">${product.price}</span></div>
+        <div class="cell">${item.amount}</div>
+        <div class="cell"><span class="product-subtotal price">0</span></div>
+        <div class="cell"><a href="#!" class="fas fa-trash-alt"></a></div>
+    </div>
+    `;
+
+    const findItem = (items, id) => items.find(item => item.id == id);
+    this.populateShoppingCart = (products) => {
+        let result = `
+        <div class="row header">
+                        <div class="cell">Cover</div>
+                        <div class="cell">Product</div>
+                        <div class="cell">Price</div>
+                        <div class="cell">Quantity</div>
+                        <div class="cell">Total</div>
+                        <div class="cell">Action</div>
+        </div>`;
+        cart.forEach(item => result += cartItemTemplate(item, findItem(products, item.id)));
+        return result;
+    }
+
+    this.setCartTotal = function(shoppingCartItems) {
+        let tmpTotal = 0;
+        let subTotal = 0;
+
+        cart.map(item => {
+            let price = shoppingCartItems.querySelector(`#id${item.id} .product-price`).textContent;
+            tmpTotal = +price * item.amount;
+            shoppingCartItems.querySelector(`#id${item.id} .product-subtotal`).textContent = parseFloat(tmpTotal).toFixed(2);
+            subTotal += parseFloat(tmpTotal).toFixed(2);
+
+        });
+
+        document.querySelector('.cart-subtotal').textContent = this.totalInCart();
+        document.querySelector('.cart-tax').textContent = this.tax;
+        document.querySelector('.cart-shipping').textContent = this.shipping;
+        document.querySelector('.cart-total').textContent = (+this.totalInCart() + +this.tax + +this.shipping).toFixed(2);
+    }
+    // 
     this.addItemToCart = function(product) {
 
         let inCart = cart.some(item => item.id === product.id);
 
-        if(inCart) {
+        if (inCart){
             let index = cart.findIndex(item => item.id === product.id);
             cart[index].amount += product.amount;
-        } else {
+        }else{
             let item = new Item(product.id, product.price, product.amount);
             cart.push(item);
         }
-
         this.saveCart();
     }
 
@@ -202,32 +250,32 @@ function Cart(tax = 0.07, shipping = 0) {
 
 }
 
+
 const starsTemplate = (n) => Array(n).fill('&starf;').concat(Array(5 - n).fill('&star;')).join('');
 
 function ProductList(products) {
-
+    
     this.products = products;
 
     this.productTemplate = (product) => `
-    <article class="product" data-id="${product.id}">
-        <div class="icons">
-            <a href="#!" class="fas fa-shopping-cart add-to-cart"></a>
-            <a href="#!" class="fas fa-heart add-to-wishlist"></a>
-            <a href="#!" class="fas fa-eye show-details"></a>
-        </div>
-        <div class="image">
-            <div class="badge bg-${product.badge.bg}">${product.badge.title}</div>
-            <img src="${product.image}" alt="${product.name}">
-        </div>
-        <div class="content" data-id="${product.id}">
-            <h3 class="product-name">${product.name}</h3>
-            <span> <span class="price product-price">${product.price}</span></span> <span class="stars">${starsTemplate(product.stars)}</span> 
-        </div>
-    </article>
-    `;
+        <article class="product" data-id="${product.id}">
+            <div class="icons">
+                <a href="#!" class="fas fa-shopping-cart add-to-cart"></a>
+                <a href="#!" class="fas fa-heart add-to-wishlist"></a>
+                <a href="#!" class="fas fa-eye show-details"></a>
+            </div>
+            <div class="image">
+                <div class="badge bg-${product.badge.bg}">${product.badge.title}</div>
+                <img src="${product.image}" alt="${product.name}">
+            </div>
+            <div class="content" data-id="${product.id}">
+                <h3 class="product-name">${product.name}</h3>
+                <span><span class="price"></span><span class="price product-price">${product.price}</span></span> <span class="starf">${starsTemplate(product.stars)}</span>
+            </div>                    
+        </article>`;
 
-    this.populateProductList = function(products) {
-        let content = '';
+    this.populateProductList = function (products) {
+        let content = "";
         products.forEach(item => content += this.productTemplate(item))
         return content;
     }
@@ -235,7 +283,7 @@ function ProductList(products) {
     this.getProductById = (id) => this.products.find(item => item.id == id);
 }
 
-const liElement = (item) => `<li><a class="category-item href="#!" data-id="${item.id}">${item.name}</a></li>`;
+const liElement = (item) => `<li><a class="category-item" href="#!" data-id="${item.id}">${item.name}</a></li>`;
 
 const ulElement = items => {
     let ul = document.createElement('ul');
@@ -245,7 +293,6 @@ const ulElement = items => {
     for (let item of items) {
         res += liElement(item);
     }
-
     ul.innerHTML = res;
     return ul;
 }
@@ -255,6 +302,7 @@ const distinctSections = items => {
     let unique = [...new Set(mapped)];
     return unique;
 }
+
 
 function categoriesCollation(distinct, categories) {
     let result = [];
@@ -280,16 +328,18 @@ const populateCategories = (categoryContainer, categories) => {
         categoryContainer.append(sectionName(distinct[i]));
         categoryContainer.append(ulElement(collation[i]));
     }
+
 }
+
 
 function renderCategory(productContainer, selector, products) {
     const categoryItems = document.querySelectorAll(selector);
 
-    categoryItems.forEach(item => item.addEventListener('click', event => {
-        event.preventDefault();
+    categoryItems.forEach(item => item.addEventListener('click', e => {
+        e.preventDefault();
 
-        if (event.target.classList.contains('category-item')) {
-            const category = event.target.dataset.id;
+        if (e.target.classList.contains('category-item')) {
+            const category = e.target.dataset.id;
             const categoryFilter = items => items.filter(item => item.category == category);
             productContainer.innerHTML = productList.populateProductList(categoryFilter(products));
         } else {
@@ -297,11 +347,10 @@ function renderCategory(productContainer, selector, products) {
         }
 
         let productCards = productContainer.querySelectorAll('.product');
-        productCards.forEach(item => CardProduct(item));
+        productCards.forEach(item => new CardProduct(item));
 
     }))
 }
-
 
 const sortingOptions = () => sortingOrders.map(item => `<option value="${item.key}">${item.value}</option>`).join('');
 
@@ -313,27 +362,100 @@ function renderSelect(selectPicker, products, productContainer) {
             case 'low-high':
                 productContainer.innerHTML = productList.populateProductList(products.sort(compare('price', 'asc')))
                 break;
-            case 'high-low' :
+            case 'high-low':
                 productContainer.innerHTML = productList.populateProductList(products.sort(compare('price', 'desc')))
                 break;
-            case 'popularity' :
+            case 'popularity':
                 productContainer.innerHTML = productList.populateProductList(products.sort(compare('stars', 'desc')))
                 break;
+
             default:
                 productContainer.innerHTML = productList.populateProductList(products.sort(compare('id', 'asc')))
+            
         }
         let productCards = productContainer.querySelectorAll('.product');
         productCards.forEach(item => new CardProduct(item));
+
     });
 }
 
-let shoppingCart = new Cart();
-let productList = new ProductList(products);
 
+function Store() {
+
+    this.init = function(key) {
+        if(!this.isset(key)) {
+            this.set(key, []);
+        }
+        return this.get(key);
+    }
+
+    this.isset = function(key) {
+        return this.get(key) !== null;
+    }
+
+    this.get = function(key) {
+        let value = localStorage.getItem(key);
+        return value === null ? null : JSON.parse(value);
+    }
+
+    this.set = function(key, value) {
+        return localStorage.setItem(key, JSON.stringify(value));
+    }
+    
+}
+
+
+const badgeTemplate = (item) => `
+<div class="form-check mb-1">
+<input class="form-check-input" type="checkbox" id="id-${item}" value="${item}" name="badge">
+&nbsp;<label class="form-check-label" for="id-${item}">${item}</label>
+</div>
+`;
+
+const renderList = (products, value) => productList.populateProductList(products.filter(product => product.badge.title.includes(value)));
+
+const renderShowOnly = (showOnly, products, productContainer) => {
+    let badges = [...new Set([...products.map(item => item.badge.title)].filter(item=>item != ''))];
+
+    showOnly.innerHTML = badges.map(item => badgeTemplate(item)).join("");
+    let checkboxes = showOnly.querySelectorAll('input[name="badge"]');
+
+    let values = [];
+
+    checkboxes.forEach(item => {
+        item.addEventListener("change", e => {
+            if(e.target.checked) {
+                values.push(item.value);
+                productContainer.innerHTML = values.map(value => renderList(products, value)).join("");
+            }else{
+                if (values.length != 0) {
+                    values.pop(item.value)
+                    productContainer.innerHTML = values.map(value => renderList(products, value)).join("");
+                }
+            }
+            if (values.length == 0) {
+                productContainer.innerHTML = productList.populateProductList(products);
+            }
+            let productCards = productContainer.querySelectorAll('.product');
+            productCards.forEach(item => new CardProduct(item));
+
+        }) 
+    })
+}
+
+
+let shoppingCart =  new Cart();
+let productList = new ProductList(products);
+const cartAmount = document.getElementById('cart-amount');
+
+cartAmount.textContent = shoppingCart.totalAmount();
 function main() {
+
     const productContainer = document.querySelector('.product-container');
 
-    productContainer.innerHTML = productList.populateProductList(products);
+    if (productContainer) {
+
+    productContainer.innerHTML = productList.populateProductList(products);	
 
     let productCards = productContainer.querySelectorAll('.product');
 
@@ -346,6 +468,7 @@ function main() {
         populateCategories(categoryContainer, categories);
 
         renderCategory(productContainer, '#category-container', products)
+
     }
 
     const selectPicker = document.getElementById('selectpicker');
@@ -353,82 +476,127 @@ function main() {
         renderSelect(selectPicker, products, productContainer);
     }
 
+    const showOnly = document.querySelector('.show-only');
+    if(showOnly) {
+        renderShowOnly(showOnly, products, productContainer);
+    }
 }
 
-const template = document.createElement("template");
+    const cartPage = document.getElementById('cart-page');
+    if(cartPage) {
+        const shippingCartItems = cartPage.querySelector('.cart-main .table');
+        shippingCartItems.innerHTML = shoppingCart.populateShoppingCart(products);
+        shoppingCart.setCartTotal(shippingCartItems);
+    }
+
+    const checkoutPage = document.getElementById('checkout-page');
+    if (checkoutPage) {
+        const checkoutForm = checkoutPage.getElementById('checkout-form');
+        const errorMessages = document.getElementById('errorMessages');
+        function displayError(message) {
+            errorMessages.innerHTML += `<div class="error">${message}</div>`;
+        }
+
+        function isValidEmail(email) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        }
+
+        checkoutForm.addEventListener('submit', function (event) {
+            event.preventDefault();
+
+            const {name, email, address1, address2, city, zipcode} = checkoutForm.elements;
+
+            errorMessages.innerHTML = "";
+            if (!name.value.trim()) {
+                displayError('Name field is required.')
+                return;
+            }
+            if (!address1.value.trim() || !address2.value.trim()) {
+                displayError('Address 1 or Address 2 fields is required.')
+                return;
+            }
+
+            if (!city.value.trim()) {
+                displayError('City field is required.')
+                return;
+            }
+            if (!zipcode.value.trim()) {
+                displayError('City postal code is required.')
+                return;
+            }
+
+            if (!email.value.trim() || !isValidEmail(email.value)) {
+                displayError('Please enter a valid email address.')
+                return;
+            }
+
+        })
+
+    }
+
+
+}
+
+const template = document.createElement('template');
+
 template.innerHTML = `
 <footer class="mb-3">
-      <div class="container page-footer">
-        <section class="footer-main">
-          <div class="footer-main-item">
-            <h3 class="footer-title">About</h3>
-            <ul>
-              <li><a href="">Services</a></li>
-              <li><a href="">Profiles</a></li>
-              <li><a href="">Priceas</a></li>
-              <li><a href="">Customers</a></li>
-              <li><a href="">Careers</a></li>
-              <li><a href="/exercises/index.html">Exercises</a></li>
-            </ul>
-          </div>
+        <div class="container page-footer">
+            <section class="footer-main">
+                <div class="footer-main-item">
+                    <h3 class="footer-title">About</h3>
+                    <ul>
+                        <li><a href="">Services</a></li>
+                        <li><a href="">Profiles</a></li>
+                        <li><a href="">Priceas</a></li>
+                        <li><a href="">Customers</a></li>
+                        <li><a href="./exercises/index.html">Exercises</a></li>
+                    </ul>
+                </div>
+                <div class="footer-main-item">
+                    <h3 class="footer-title">Resources</h3>
+                    <ul>
+                        <li><a href="">Docs</a></li>
+                        <li><a href="">Blog</a></li>
+                        <li><a href="">eBooks</a></li>
+                        <li><a href="">Webinars</a></li>
+                    </ul>
+                </div>
+                <div class="footer-main-item">
+                    <h3 class="footer-title">Contact</h3>
+                    <ul>
+                        <li><a href="">Help</a></li>
+                        <li><a href="">Sales</a></li>
+                        <li><a href="">Advertise</a></li>
+                    </ul>
+                </div>
+            </section>
 
-          <div class="footer-main-item">
-            <h3 class="footer-title">Resources</h3>
-            <ul>
-              <li><a href="">Docs</a></li>
-              <li><a href="">Blog</a></li>
-              <li><a href="">eBooks</a></li>
-              <li><a href="">Webinars</a></li>
-            </ul>
-          </div>
-
-          <div class="footer-main-item">
-            <h3 class="footer-title">Contact</h3>
-            <ul>
-              <li><a href="">Help</a></li>
-              <li><a href="">Sales</a></li>
-              <li><a href="">Advertise</a></li>
-            </ul>
-          </div>
-        </section>
-
-        <!-- Footer-social -->
-        <section class="footer-social py-3">
-          <ul class="footer-social-list">
-            <li>
-              <a href=""><i class="fab fa-facebook"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="fab fa-twitter"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="fab fa-instagram"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="fab fa-github"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="fab fa-linkedin"></i></a>
-            </li>
-            <li>
-              <a href=""><i class="fab fa-youtube"></i></a>
-            </li>
-          </ul>
-        </section>
-
-        <!-- Footer ligal -->
-        <section class="footer-ligal">
-          <ul class="footer-ligal-list">
-            <li><a href="#">Terms &amp; Conditions</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">&copy; 2024 Copyright Shopaholic Inc.</a></li>
-          </ul>
-        </section>
-      </div>
+            <section class="footer-social py-3">
+                <ul class="footer-social-list">
+                    <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                    <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                    <li><a href=""><i class="fab fa-instagram"></i></a></li>
+                    <li><a href=""><i class="fab fa-github"></i></a></li>
+                    <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                    <li><a href=""><i class="fab fa-youtube"></i></a></li>
+                </ul>
+            </section>
+           
+            <section class="footer-ligal">
+                <ul class="footer-ligal-list">
+                    <li><a href="#">Terms &amp; Conditions</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">&copy; 2024 Copyright Shopaholic Inc.</a></li>
+                </ul>
+                
+            </section>
+        </div>
     </footer>
 `;
+
 let clone = template.content.cloneNode(true);
-document.body.appendChild(clone);
+document.body.appendChild(clone)
 
 if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', () => {
