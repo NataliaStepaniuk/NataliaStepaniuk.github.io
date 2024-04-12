13c5d106d0fa1244a502bf82cefcd01b070ba7f1
Natalia Stepaniuk <neteliy84@gmail.com>
2024-04-12 17:10:09 +0300

    exercises-31

--- exercises/lecture-31/app.js
+++ exercises/lecture-31/app.js
@@ -1,9 +1,9 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
 
-const template = (item, authorName) => `
+const template = (item) => `
 <h3>${item.title}</h3>
 <div>${item.body}</div>
-<p>Author: <strong><span class="author" data-id="${item.userId}">${authorName}</span></strong></p>
+<p>Author: <strong><span class="author" data-id="${item.userId}"></stap></strong></p>
 `;
 
 const xhrPromise = (method, url) => {
@@ -11,7 +11,7 @@ const xhrPromise = (method, url) => {
     const xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.send();
-
+  
     xhr.onload = () => {
       if (xhr.status >= 400) {
         reject(xhr.response);
@@ -21,41 +21,37 @@ const xhrPromise = (method, url) => {
     };
 
     xhr.onerror = () => {
-      reject("Something went wrong!");
-    };
+      reject('Something went wrong!');
+    };   
   });
 
   return promise;
 };
 
-const usersCache = {};
-
 xhrPromise("GET", url)
-  .then((response) => JSON.parse(response))
-  .then((posts) => {
-    const promises = posts.map((post) => {
-      if (usersCache[post.userId]) {
-        return Promise.resolve(usersCache[post.userId]);
-      } else {
-        return xhrPromise(
-          "GET",
-          `https://jsonplaceholder.typicode.com/users/${post.userId}`
-        ).then((response) => {
-          const user = JSON.parse(response);
-          usersCache[post.userId] = user.name;
-          return user.name;
-        });
-      }
-    });
-
-    return Promise.all(promises);
-  })
-  .then((authorNames) => {
-    const result = authorNames.reduce((acc, authorName, index) => {
-      const post = post[index];
-      return acc + template(post, authorName);
-    }, "");
-
+.then(response => {
+    const posts = JSON.parse(response)
+		let result = ''
+    posts.forEach(item => {
+        result += template(item)
+    })
     document.getElementById("blog").innerHTML = result;
+
+})
+.then(() => {
+  const users = document.querySelectorAll('.author')
+  users.forEach(user => {
+      xhrPromise('GET', `https://jsonplaceholder.typicode.com/users/${user.dataset.id}`)
+      .then(response => {
+          let userName = JSON.parse(response)
+          user.textContent = userName.name
+      })
   })
-  .catch((error) => console.error(error));
+})
+
+
+    
+
+
+
+
