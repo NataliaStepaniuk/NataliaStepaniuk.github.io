cda9b32dc3b4853b9b543483b6db17f620e47a2c
Natalia Stepaniuk <neteliy84@gmail.com>
2024-04-13 12:44:41 +0300

    fixed the error

--- exercises/lecture-31/app.js
+++ exercises/lecture-31/app.js
@@ -1,9 +1,9 @@
-const url = "https://jsonplaceholder.typicode.com/posts";
+/*const url = "https://jsonplaceholder.typicode.com/posts";
 
 const template = (item) => `
 <h3>${item.title}</h3>
 <div>${item.body}</div>
-<p>Author: <strong><span class="author" data-id="${item.userId}"></stap></strong></p>
+<p>Author: <strong><span class="author" data-id="${item.userId}"></span></strong></p>
 `;
 
 const xhrPromise = (method, url) => {
@@ -47,8 +47,68 @@ xhrPromise("GET", url)
           user.textContent = userName.name
       })
   })
-})
+})*/
 
+const urlPosts = "https://jsonplaceholder.typicode.com/posts";
+const urlUsers = "https://jsonplaceholder.typicode.com/users";
+
+const template = (item, user) => `
+  <h3>${item.title}</h3>
+  <div>${item.body}</div>
+  <p>Author: <strong><span class="author" data-id="${item.userId}">${user.name}</span></strong></p>
+`;
+
+const xhrPromise = (method, url) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.send();
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(JSON.parse(xhr.response));
+      }
+    };
+
+    xhr.onerror = () => {
+      reject('Something went wrong!');
+    };
+  });
+
+  return promise;
+};
+
+
+const userCache = {};
+
+xhrPromise("GET", urlPosts)
+  .then(posts => {
+    const userPromises = posts.map(post => {
+      if (userCache[post.userId]) {
+        return Promise.resolve(userCache[post.userId]); 
+      } else {
+        return xhrPromise("GET", `${urlUsers}/${post.userId}`); 
+      }
+    });
+
+    Promise.all(userPromises)
+      .then(users => {
+        users.forEach((user, index) => {
+          userCache[user.id] = user; 
+        });
+
+        const result = posts.map((post, index) => template(post, users[index]));
+        document.getElementById("blog").innerHTML = result.join('');
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
     
 
